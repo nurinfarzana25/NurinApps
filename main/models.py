58ba85ce722c8cdf97a3932de68cd26c46234c80
nurinfarzana25 <nurinfarzana25@gmail.com>
2023-09-12 09:29:55 +0700

    okee

---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
+    category = models.TextField()
+    
+    
+
